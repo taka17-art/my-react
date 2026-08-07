8db36b0b5c1300b0e3ad93b4cfd0153def32ba1c
TAKA <takashitarumi17@gmail.com>
2026-08-07 09:40:35 +0900

    Refactor application structure and remove unused assets
    - Deleted unused SVG icons and image assets to clean up the project.
    - Refactored App component to use Header and Home components for better modularity.
    - Created Header and Home components with corresponding styles.
    - Updated main.jsx to reflect changes in App component export.
    - Removed PostList component as it is no longer needed.

---
+++ src/components/Home/main.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { posts } from "../../data/posts";
+import classes from "./Home.module.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <main className={classes.container}>
+      {/* ここに「記事一覧」を追加*/}
+      <h2 className={classes.title}>記事一覧</h2>
+      <ul className={classes.container}>
+        {posts.map((post) => {
+          return (
+            <li key={post.id} className={classes.list}>
+              <a href={post.thumbnailUrl} className={classes.link}>
+                <div className={classes.post}>
+                  <div className={classes.postContent}>
+                    <div className={classes.postInfo}>
+                      <div className={classes.postDate}>
+                        {new Date(post.createdAt).toLocaleDateString()}
+                      </div>
+                      <div className={classes.postCategories}>
+                        {post.categories.map((category, id) => {
+                          return (
+                            <p key={id} className={classes.postCategory}>
+                              {category}
+                            </p>
+                          );
+                        })}
+                      </div>
+                    </div>
+                    <p className={classes.postTitle}>{post.title}</p>
+                    <div
+                      className={classes.postBody}
+                      dangerouslySetInnerHTML={{ __html: post.content }}
+                    />
+                    <div />
+                  </div>
+                </div>
+              </a>
+            </li>
+          );
+        })}
+      　</ul>
+    　</main>
+  　</div>
+  );
+};

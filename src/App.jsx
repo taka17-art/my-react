3dfdb7dafc468286bf01b9908589a161c93e330c
TAKA <takashitarumi17@gmail.com>
2026-08-08 12:47:45 +0900

    インデントの修正とh1タグへの変更

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Home } from "./components/Home/main";
 import { Routes, Route } from "react-router-dom";
 import { Detail as ArticleDetail } from "./components/Detail/main";
 
-
 export const App = () => {
   return (
     <div>
--- src/components/Detail/main.jsx
+++ src/components/Detail/main.jsx
@@ -6,41 +6,41 @@ export const Detail = () => {
   const { id } = useParams();
   const post = posts.find((post) => post.id === Number(id));
 
-    if (!post) return <div>記事が見つかりませんでした。</div>;
+  if (!post) return <div>記事が見つかりませんでした。</div>;
 
-    return (
-        <div className={classes.container}>
-            <div className={classes.post}>
-                <div className={classes.postImage}>
-                    <img src={post.thumbnailUrl} alt={post.title}/>
-                    </div>
-                <div className={classes.postContent}>
-                    <div className={classes.postInfo}>
-                        <div className={classes.postDate}>
-                            {new Date(post.createdAt).toLocaleDateString()}
-                        </div>
-                        <div className={classes.postCategories}>
-                            {post.categories.map((category, index) => {
-                                return (
-                                    <p key={index} className={classes.postCategory}>
-                                        {category}
-                                    </p>
-                                );
-                            })}
-                        </div>
-                    </div>
-                    <p className={classes.postTitle}>{post.title}</p>
-                    <div
-                        className={classes.postBody}
-                        dangerouslySetInnerHTML={{ __html: post.content }}
-                    />
-                    <div className={classes.backLinkContainer}>
-                     <Link to="/" className={classes.backLink}>
-                     記事一覧へ戻る
-                     </Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={classes.container}>
+      <div className={classes.post}>
+        <div className={classes.postImage}>
+          <img src={post.thumbnailUrl} alt={post.title} />
         </div>
-    );
+        <div className={classes.postContent}>
+          <div className={classes.postInfo}>
+            <div className={classes.postDate}>
+              {new Date(post.createdAt).toLocaleDateString()}
+            </div>
+            <div className={classes.postCategories}>
+              {post.categories.map((category, index) => {
+                return (
+                  <p key={index} className={classes.postCategory}>
+                    {category}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+          <h1 className={classes.postTitle}>{post.title}</h1>
+          <div
+            className={classes.postBody}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+          <div className={classes.backLinkContainer}>
+            <Link to="/" className={classes.backLink}>
+              記事一覧へ戻る
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };

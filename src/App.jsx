9dca5a4553d946971e9159f0b92e84aaa5ac0c5a
TAKA <takashitarumi17@gmail.com>
2026-08-10 11:29:27 +0900

    課題４の実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header/main";
 import { Home } from "./components/Home/main";
 import { Detail } from "./components/Detail/main";
+import { Contact } from "./components/Contact/main"; 
 
 export const App = () => {
   return (
-    <div>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts/:id" element={<Detail />} />
+        <Route path="/contact" element={<Contact />} /> 
       </Routes>
     </>
-
   );
 };
-
